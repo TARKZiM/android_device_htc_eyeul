7c0bfd375295d6a2b32a5e80b56ff173617f5aca
Bruno Martins <bgcngm@gmail.com>
2019-08-07 01:02:18 +0100

    eyeul: camera: "Fork" slightly trimmed libcamera_client
    As a preparation for creating a custom libcamera_client that will
    act like a shim, fork only the files that must be adapted and
    simply symlink almost everything else from frameworks/av/camera.
    
    Change-Id: Ie9297b17addb19f46992815dea84dfc0239e3fa4

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,30 +1,26 @@
-# Copyright (C) 2015 The CyanogenMod Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+ 
 LOCAL_PATH := $(call my-dir)
-
 include $(CLEAR_VARS)
 
-LOCAL_C_INCLUDES := \
-    frameworks/av/include
+LOCAL_C_INCLUDES += \
+    $(LOCAL_PATH)/../include \
+    system/media/camera/include
 
 LOCAL_SRC_FILES := \
-    CameraParameters.cpp \
-    CameraParameters_EXT.cpp
+    CameraWrapper.cpp
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
-LOCAL_MODULE := libcamera_parameters_ext
+LOCAL_STATIC_LIBRARIES := libbase libarect
+LOCAL_SHARED_LIBRARIES := \
+    libhardware liblog libcamera_client libutils libcutils libdl \
+    android.hidl.token@1.0-utils \
+    android.hardware.graphics.bufferqueue@1.0
+
+LOCAL_HEADER_LIBRARIES := libnativebase_headers
+
+LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE_TAGS := optional
+LOCAL_PROPRIETARY_MODULE := true
 
-include $(BUILD_STATIC_LIBRARY)
+include $(BUILD_SHARED_LIBRARY)
+#include $(BUILD_HEAPTRACKED_SHARED_LIBRARY)
--- camera/CameraParameters.cpp
+++ camera/CameraParameters.cpp
@@ -21,7 +21,6 @@
 #include <string.h>
 #include <stdlib.h>
 #include <camera/CameraParameters.h>
-#include <camera/CameraParametersExtra.h>
 #include <system/graphics.h>
 
 namespace android {
@@ -107,7 +106,6 @@ const char CameraParameters::WHITE_BALANCE_DAYLIGHT[] = "daylight";
 const char CameraParameters::WHITE_BALANCE_CLOUDY_DAYLIGHT[] = "cloudy-daylight";
 const char CameraParameters::WHITE_BALANCE_TWILIGHT[] = "twilight";
 const char CameraParameters::WHITE_BALANCE_SHADE[] = "shade";
-const char CameraParameters::WHITE_BALANCE_MANUAL_CCT[] = "manual-cct";
 
 // Values for effect settings.
 const char CameraParameters::EFFECT_NONE[] = "none";
@@ -170,66 +168,13 @@ const char CameraParameters::FOCUS_MODE_FIXED[] = "fixed";
 const char CameraParameters::FOCUS_MODE_EDOF[] = "edof";
 const char CameraParameters::FOCUS_MODE_CONTINUOUS_VIDEO[] = "continuous-video";
 const char CameraParameters::FOCUS_MODE_CONTINUOUS_PICTURE[] = "continuous-picture";
-const char CameraParameters::FOCUS_MODE_MANUAL_POSITION[] = "manual";
 
 // Values for light fx settings
 const char CameraParameters::LIGHTFX_LOWLIGHT[] = "low-light";
 const char CameraParameters::LIGHTFX_HDR[] = "high-dynamic-range";
 
-// HTC settings
-const char CameraParameters::KEY_TIME_CONS_POST_PROCESSING[] = "time-cons-post-processing";
-const char CameraParameters::KEY_OIS_MODE[] = "ois_mode";
-const char CameraParameters::KEY_APP_OIS_SETTING[] = "ois-setting";
-const char CameraParameters::KEY_OIS_SUPPORT[] = "ois_support";
-const char CameraParameters::KEY_CONTIBURST_TYPE[] = "contiburst-type";
-const char CameraParameters::KEY_CAPTURE_MODE[] = "capture-mode";
-const char CameraParameters::CAPTURE_MODE_NORMAL[] = "normal";
-const char CameraParameters::CAPTURE_MODE_CONTI_ZOE[] = "contizoe";
-const char CameraParameters::CAPTURE_MODE_CONTI_BURST[] = "contiburst";
-const char CameraParameters::CAPTURE_MODE_CONTI_BURST_ONE_SHOT[] = "contiburst-one-shot";
-const char CameraParameters::CAPTURE_MODE_HDR[] = "hdr";
-const char CameraParameters::CAPTURE_MODE_PANORAMA[] = "panorama";
-const char CameraParameters::CAPTURE_MODE_ZOE[] = "zoe";
-const char CameraParameters::CAPTURE_MODE_EIS[] = "eis";
-const char CameraParameters::KEY_CONTI_BURST_STATE[] = "contiburst-state";
-const char CameraParameters::KEY_SUPPORTED_CAPTURE_MODES[] = "capture-mode-values";
-const char CameraParameters::KEY_MIN_CONTRAST[] = "contrast-min";
-const char CameraParameters::KEY_DEF_CONTRAST[] = "contrast-def";
-const char CameraParameters::KEY_MIN_SHARPNESS[] = "sharpness-min";
-const char CameraParameters::KEY_DEF_SHARPNESS[] = "sharpness-def";
-const char CameraParameters::KEY_MIN_SATURATION[] = "saturation-min";
-const char CameraParameters::KEY_DEF_SATURATION[] = "saturation-def";
-const char CameraParameters::KEY_SINGLE_ISP_OUTPUT_ENABLED[] = "single-isp-output-enabled";
-const char CameraParameters::POST_PROCESSING_ENABLE[] = "enable";
-const char CameraParameters::POST_PROCESSING_BYPASS[] = "bypass";
-const char CameraParameters::POST_PROCESSING_DELAY[] = "delay";
-const char CameraParameters::SCENE_MODE_OFF[] = "off";
-const char CameraParameters::SCENE_MODE_TEXT[] = "text";
-const char CameraParameters::BURST_MODE_LIMIT20[] = "limit-20";
-const char CameraParameters::BURST_MODE_UNLIMITED[] = "unlimited";
-const char CameraParameters::OIS_MODE_OFF[] = "off";
-const char CameraParameters::OIS_MODE_ON[] = "on";
-const char CameraParameters::CONTI_BURST_CAPTURING[] = "contiburst-capturing";
-const char CameraParameters::CONTI_BURST_CAPTURE_DONE[] = "contiburst-done";
-const char CameraParameters::APP_OIS_SETTING_FALSE[] = "false";
-const char CameraParameters::APP_OIS_SETTING_TRUE[] = "true";
-const char CameraParameters::KEY_GPU_EFFECT[] = "GPU-effect";
-const char CameraParameters::KEY_GPU_EFFECT_PARAM_0[] = "GE-param0";
-const char CameraParameters::KEY_GPU_EFFECT_PARAM_1[] = "GE-param1";
-const char CameraParameters::KEY_GPU_EFFECT_PARAM_2[] = "GE-param2";
-const char CameraParameters::KEY_GPU_EFFECT_PARAM_3[] = "GE-param3";
-const char CameraParameters::KEY_FORCE_USE_AUDIO_ENABLED[] = "forceuseaudio";
-const char CameraParameters::KEY_ZSL[] = "zsl";
-const char CameraParameters::KEY_CAMERA_MODE[] = "camera-mode";
-const char CameraParameters::KEY_SMILEINFO_BYFACE_SUPPORTED[] = "smileinfo-byface-supported";
-const char CameraParameters::ZSL_OFF[] = "off";
-
-#ifdef CAMERA_PARAMETERS_EXTRA_C
-CAMERA_PARAMETERS_EXTRA_C
-#endif
-
 CameraParameters::CameraParameters()
-    : CameraParameters_EXT(this), mMap()
+                : mMap()
 {
 }
 
@@ -305,14 +250,6 @@ void CameraParameters::set(const char *key, const char *value)
         //XXX ALOGE("Value \"%s\"contains invalid character (= or ;)", value);
         return;
     }
-#ifdef QCOM_HARDWARE
-    // qcom cameras default to delivering an extra zero-exposure frame on HDR.
-    // The android SDK only wants one frame, so disable this unless the app
-    // explicitly asks for it
-    if (!get("hdr-need-1x")) {
-        mMap.replaceValueFor(String8("hdr-need-1x"), String8("false"));
-    }
-#endif
 
     mMap.replaceValueFor(String8(key), String8(value));
 }
@@ -592,7 +529,7 @@ int CameraParameters::previewFormatToEnum(const char* format) {
         !strcmp(format, PIXEL_FORMAT_RGBA8888) ?
             HAL_PIXEL_FORMAT_RGBA_8888 :    // RGB8888
         !strcmp(format, PIXEL_FORMAT_BAYER_RGGB) ?
-            HAL_PIXEL_FORMAT_RAW_SENSOR :   // Raw sensor data
+            HAL_PIXEL_FORMAT_RAW16 :   // Raw sensor data
         -1;
 }
 
@@ -600,15 +537,4 @@ bool CameraParameters::isEmpty() const {
     return mMap.isEmpty();
 }
 
-
-void CameraParameters::getBrightnessLumaTargetSet(int *magic, int *sauce) const{};
-void CameraParameters::setBrightnessLumaTargetSet(int brightness, int luma) {
-    char str[32];
-    snprintf(str, sizeof(str),"%d,%d", brightness, luma);
-    set("brightness-luma-target-set", str);
-};
-void CameraParameters::getRawSize(int *magic, int *sauce) const{};
-void CameraParameters::setZsl(const char *sauce) { set("zsl",sauce);};
-const char *CameraParameters::getZsl() const { return get("zsl");};
-
-};
+}; // namespace android
